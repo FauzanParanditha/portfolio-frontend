6eaa9f08b5d029f7a49ec676576790963060fe42
Fauzan Paranditha <paranditha@gmail.com>
2025-12-10 08:19:45 +0700

    feat: jwt config

---
+++ src/utils/var.ts
@@ -0,0 +1,8 @@
+export const jwtConfig = {
+  admin: {
+    accessTokenExpiresIn: "7h",
+    refreshTokenExpiresIn: "7d",
+    accessTokenName: "_aPDsbTkn",
+    refreshTokenName: "_aPrDsbTkn",
+  },
+};
